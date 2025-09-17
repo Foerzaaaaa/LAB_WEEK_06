2925e6c2bde627af9180deb3518749c3ebb3c70d
Ahmad Dhavi <ahmaddhavi21@gmail.com>
2025-09-17 18:17:18 +0700

    Commit No.3 - Deleting an Item from RecyclerView by Swiping

--- app/src/main/java/com/example/lab_week_06/CatAdapter.kt
+++ app/src/main/java/com/example/lab_week_06/CatAdapter.kt
@@ -3,9 +3,11 @@ package com.example.lab_week_06
 import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.RecyclerView
 import com.example.lab_week_06.model.CatModel
 
+@Suppress("DEPRECATION")
 class CatAdapter(
     private val layoutInflater: LayoutInflater,
     private val imageLoader: ImageLoader,
@@ -21,6 +23,11 @@ class CatAdapter(
         notifyDataSetChanged()
     }
 
+    fun removeItem(position: Int) {
+        cats.removeAt(position)
+        notifyItemRemoved(position)
+    }
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CatViewHolder {
         val view = layoutInflater.inflate(R.layout.item_list, parent, false)
         return CatViewHolder(view, imageLoader, onClickListener)
@@ -34,5 +41,20 @@ class CatAdapter(
 
     interface OnClickListener {
         fun onItemClick(cat: CatModel)
+    }
+
+    inner class SwipeToDeleteCallback :
+        ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT) {
+
+        override fun onMove(
+            recyclerView: RecyclerView,
+            viewHolder: RecyclerView.ViewHolder,
+            target: RecyclerView.ViewHolder
+        ): Boolean = false
+
+        override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+            val position = viewHolder.adapterPosition
+            removeItem(position)
+        }
     }
 }
