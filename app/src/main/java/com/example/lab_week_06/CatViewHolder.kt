81c3d22becf22fdff205f4f8499334cd41ad5e0b
Ahmad Dhavi <ahmaddhavi21@gmail.com>
2025-09-17 18:15:25 +0700

    Commit No.2 - Clickable RecyclerView

--- app/src/main/java/com/example/lab_week_06/CatViewHolder.kt
+++ app/src/main/java/com/example/lab_week_06/CatViewHolder.kt
@@ -4,16 +4,19 @@ import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import com.example.lab_week_06.model.CatBreed
 import com.example.lab_week_06.model.CatModel
+import com.example.lab_week_06.model.CatBreed
 import com.example.lab_week_06.model.Gender
 
-private val FEMALE_SYMBOL = "\u2640"
-private val MALE_SYMBOL = "\u2642"
+private const val FEMALE_SYMBOL = "\u2640"
+private const val MALE_SYMBOL = "\u2642"
 private const val UNKNOWN_SYMBOL = "?"
 
-class CatViewHolder(containerView: View, private val imageLoader: ImageLoader)
-    : RecyclerView.ViewHolder(containerView) {
+class CatViewHolder(
+    private val containerView: View,
+    private val imageLoader: ImageLoader,
+    private val onClickListener: CatAdapter.OnClickListener
+) : RecyclerView.ViewHolder(containerView) {
 
     private val catBiographyView: TextView = containerView.findViewById(R.id.cat_biography)
     private val catBreedView: TextView = containerView.findViewById(R.id.cat_breed)
@@ -22,14 +25,18 @@ class CatViewHolder(containerView: View, private val imageLoader: ImageLoader)
     private val catPhotoView: ImageView = containerView.findViewById(R.id.cat_photo)
 
     fun bindData(cat: CatModel) {
+        containerView.setOnClickListener { onClickListener.onItemClick(cat) }
+
         imageLoader.loadImage(cat.imageUrl, catPhotoView)
         catNameView.text = cat.name
+        catBiographyView.text = cat.biography
+
         catBreedView.text = when (cat.breed) {
             CatBreed.AmericanCurl -> "American Curl"
             CatBreed.BalineseJavanese -> "Balinese-Javanese"
             CatBreed.ExoticShorthair -> "Exotic Shorthair"
         }
-        catBiographyView.text = cat.biography
+
         catGenderView.text = when (cat.gender) {
             Gender.Female -> FEMALE_SYMBOL
             Gender.Male -> MALE_SYMBOL
